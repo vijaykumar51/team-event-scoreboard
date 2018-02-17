18ecbdc2a6a2b1d4241da048db2b481eef423834
Vijay Kumar <vijay.kumar.em51@gmail.com>
2018-02-17 23:49:17 +0530

    Adding add player functionality to team-card component

--- src/app/components/team-card/team-card.component.ts
+++ src/app/components/team-card/team-card.component.ts
@@ -1,11 +1,33 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { DataStoreService } from "../../services/data-store.service";
 
 @Component({
   selector: "app-team-card",
   styleUrls: ["./team-card.component.css"],
   templateUrl: "./team-card.component.html"
 })
-export class TeamCardComponent {
-  @Input() teamName: string;
-  @Input() teamPlayers: Array<string>;
+export class TeamCardComponent implements OnInit {
+  @Input() public teamName: string;
+  @Input() public teamPlayers: Array<string>;
+  public showInputBox: boolean = false;
+
+  constructor(private dataStoreService: DataStoreService) {}
+
+  ngOnInit() {
+    this.teamPlayers.forEach((player, index) => {
+      if (!player) {
+        this.teamPlayers.splice(index);
+      }
+    });
+  }
+
+  public addPlayer(player) {
+    this.showInputBox = false;
+    this.teamPlayers.push(player.value);
+    this.dataStoreService.teams.set(this.teamName, this.teamPlayers);
+  }
+
+  public showPlayerInputBox() {
+    this.showInputBox = true;
+  }
 }
